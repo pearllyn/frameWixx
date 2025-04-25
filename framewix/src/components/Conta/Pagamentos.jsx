21205c78867322136b49490ba994e1a4e7434f05
Pearl <142231990+pearllyn@users.noreply.github.com>
2025-04-24 23:33:49 -0300

    Update Pagamentos.jsx

--- framewix/src/components/Conta/Pagamentos.jsx
+++ framewix/src/components/Conta/Pagamentos.jsx
@@ -4,7 +4,7 @@ import styles from './styles/Pagamentos.module.css';
 function Pagamentos() {
     const { role } = useOutletContext();
 
-    if (role != 'artista') return <Navigate to="/conta/perfil" replace />
+    if (role !== 'artista') return <Navigate to="/conta/perfil" replace />
 
     return (
         <section className={styles.pagamentos_container}>
